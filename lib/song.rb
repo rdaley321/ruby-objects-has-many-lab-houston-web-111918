d8013b89d1454cb9d71ab3e0cd7593823c121bae
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 18:56:46 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def artist
+    
+  end
+end
