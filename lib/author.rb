f872129e89bf31907ce5a331c59d36cda27fbfcf
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 19:25:00 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,8 @@
 class Author
   attr_accessor :name, :posts
+  
+  @@all_posts = []
+  
   def initialize(name)
     @name = name
     @posts = []
@@ -7,12 +10,21 @@ class Author
   
   def add_post(post)
     self.posts << post
+    self.class.all_posts << post
     post.author = self
   end
   
   def add_post_by_title(title)
     new = Post.new(title)
     add_post(new)
+  end
+  
+  def all_posts
+    @@all_posts
+  end
+  
+  def post_count
+    all_posts.count
   end
   
 end
