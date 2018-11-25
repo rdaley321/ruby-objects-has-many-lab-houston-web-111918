2de5985c69e1ec7b97642243ded2182471d19f1d
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 19:43:41 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Artist
+  attr_accessor :name, :songs
+  @@all_songs = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    self.class.all_songs << song 
+    self.songs << song
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    new = Song.new(name)
+    add_song(new)
+  end
+  
+  def self.song_count
+    all_songs.count
+  end
+  
+  def self.all_songs
+    @@all_songs
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+  attr_accessor :name, :posts
+  
+  @@all_posts = []
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    self.posts << post
+    self.class.all_posts << post
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    new = Post.new(title)
+    add_post(new)
+  end
+  
+  def self.all_posts
+    @@all_posts
+  end
+  
+  def self.post_count
+    all_posts.count
+  end
+  
+end
